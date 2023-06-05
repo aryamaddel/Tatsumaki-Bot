015711222a64ec8947b0f2f4be118be7fc1837a5
Arya Maddel <maddelarya@gmail.com>
2023-06-05 16:24:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 const { token } = require('./config.json');
-const fs = require('node:fs');
+const fs = require('node:fs/promises');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
-// new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
-
 client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath, { withFileTypes: true });
 
-for (const folder of commandFolders) {
-	if (folder.isDirectory()) {
-		const commandsPath = path.join(foldersPath, folder.name);
-		const commandFiles = fs
-			.readdirSync(commandsPath, { withFileTypes: true })
-			.filter((file) => file.isFile() && file.name.endsWith('.js'));
-		for (const file of commandFiles) {
-			const filePath = path.join(commandsPath, file.name);
-			const command = require(filePath);
-			// Set a new item in the Collection with the key as the command name and the value as the exported module
-			if ('data' in command && 'execute' in command) {
-				client.commands.set(command.data.name, command);
-			}
-			else {
-				console.log(
-					`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
-				);
+async function loadCommands() {
+	const commandFolders = await fs.readdir(foldersPath, { withFileTypes: true });
+
+	for (const folder of commandFolders) {
+		if (folder.isDirectory()) {
+			const commandsPath = path.join(foldersPath, folder.name);
+			const commandFiles = (await fs.readdir(commandsPath, { withFileTypes: true }))
+				.filter((file) => file.isFile() && file.name.endsWith('.js'));
+			for (const file of commandFiles) {
+				const filePath = path.join(commandsPath, file.name);
+				let command;
+				try {
+					command = require(filePath);
+				}
+				catch (error) {
+					console.error(`[ERROR] Failed to load command at ${filePath}: ${error.message}`);
+					continue;
+				}
+				if ('data' in command && 'execute' in command) {
+					client.commands.set(command.data.name, command);
+				}
+				else {
+					console.log(
+						`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
+					);
+				}
 			}
 		}
 	}
@@ -37,7 +43,6 @@ client.once(Events.ClientReady, (c) => {
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
-	console.log(`${interaction.user.tag} entered command ${interaction.commandName} at ${new Date().toISOString()}`);
 	if (!interaction.isChatInputCommand()) return;
 	const command = interaction.client.commands.get(interaction.commandName);
 
@@ -48,23 +53,14 @@ client.on(Events.InteractionCreate, async (interaction) => {
 
 	try {
 		await command.execute(interaction);
-		console.log(`${client.user.tag} executed command ${interaction.commandName} at ${new Date().toISOString()}`);
 	}
 	catch (error) {
 		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({
-				content: 'There was an error while executing this command!',
-				ephemeral: true,
-			});
-		}
-		else {
-			await interaction.reply({
-				content: 'There was an error while executing this command!',
-				ephemeral: true,
-			});
-		}
+		await interaction.reply({
+			content: 'There was an error while executing this command!',
+			ephemeral: true,
+		});
 	}
 });
 
-client.login(token);
+loadCommands().then(() => client.login(token));
