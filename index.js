5e331b8bbeeb5a979865ea975d02fe1112ee737f
Arya Maddel <maddelarya@gmail.com>
2023-06-05 16:16:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,24 +8,26 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBit
 
 client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+const commandFolders = fs.readdirSync(foldersPath, { withFileTypes: true });
 
 for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs
-		.readdirSync(commandsPath)
-		.filter((file) => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		}
-		else {
-			console.log(
-				`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
-			);
+	if (folder.isDirectory()) {
+		const commandsPath = path.join(foldersPath, folder.name);
+		const commandFiles = fs
+			.readdirSync(commandsPath, { withFileTypes: true })
+			.filter((file) => file.isFile() && file.name.endsWith('.js'));
+		for (const file of commandFiles) {
+			const filePath = path.join(commandsPath, file.name);
+			const command = require(filePath);
+			// Set a new item in the Collection with the key as the command name and the value as the exported module
+			if ('data' in command && 'execute' in command) {
+				client.commands.set(command.data.name, command);
+			}
+			else {
+				console.log(
+					`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
+				);
+			}
 		}
 	}
 }
