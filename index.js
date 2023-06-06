c2930fe62437955bbc42dc8b2ee304d73a04528c
Arya Maddel <maddelarya@gmail.com>
2023-06-07 00:54:55 +0530

    Refactor client and loadFiles

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent],
+});
 
 function loadFiles(botClient, collectionName, folderName) {
 	const collection = new Collection();
@@ -16,27 +21,30 @@ function loadFiles(botClient, collectionName, folderName) {
 		for (const file of commandFiles) {
 			const filePath = path.join(commandsPath, file);
 			const command = require(filePath);
-			if (collectionName === 'commands') {
+			switch (collectionName) {
+			case 'commands':
 				if ('data' in command && 'execute' in command) {
 					collection.set(command.data.name, command);
 				}
 				else {
 					console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 				}
-			}
-			else if (collectionName === 'responses') {
+				break;
+			case 'responses':
 				if ('trigger' in command && 'response' in command) {
 					collection.set(command.trigger.toLowerCase(), command.response);
 				}
 				else {
 					console.log(`[WARNING] The response at ${filePath} is missing a required "trigger" or "response" property.`);
 				}
+				break;
 			}
 		}
 	}
 
 	botClient[collectionName] = collection;
 }
+
 
 loadFiles(client, 'commands', 'commands');
 loadFiles(client, 'responses', 'responses');
